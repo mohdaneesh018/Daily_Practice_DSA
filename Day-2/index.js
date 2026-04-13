2c5ee1678cd07b1f74c1d5152063655f6b030481
mohdaneesh018 <mdaneesh537@gmail.com>
2026-04-13 16:41:57 +0530

    Solved

---
+++ Day-2/index.js
@@ -0,0 +1,18 @@
+//Qs. 1. Find Second Largest Element in an array:
+
+var secondLargest = function (arr) {
+    let first = -Infinity;
+    let second = -Infinity;
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] !== first) {
+            second = arr[i];
+        }
+    }
+    return second;
+}
+console.log(secondLargest([10, 5, 8, 20, 3]));          // 10
+console.log(secondLargest([100, 52, 83, 320, 23]));     // 100
