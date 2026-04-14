80095f300de61d4fc2bd34e4411b40f1c570793e
mohdaneesh018 <mdaneesh537@gmail.com>
2026-04-14 20:37:11 +0530

    Practice

---
+++ Day-3/index.js
@@ -0,0 +1,19 @@
+// Qs. Move All Zeros in Last index:
+
+function moveZeros(arr) {
+    let index = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[index] = arr[i];
+            index++;
+        }
+    }
+    while (index < arr.length) {
+        arr[index] = 0;
+        index++;
+    }
+    return arr;
+}
+console.log(moveZeros([0, 1, 0, 3, 12]));    // [1, 3, 12, 0, 0];
+console.log(moveZeros([0, 0, 1]));           // [1, 0, 0];
