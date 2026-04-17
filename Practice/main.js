deb75256ad29178f977ce99cabf0363beeccae13
mohdaneesh018 <mdaneesh537@gmail.com>
2026-04-17 17:52:09 +0530

    Solve

---
+++ Practice/main.js
@@ -0,0 +1,16 @@
+// Qs. 1
+function secondLargest(arr) {
+    let max = -Infinity;
+    let secMax = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secMax = max;
+            max = arr[i];
+        } else if (arr[i] > secMax && arr[i] !== max) {
+            secMax = arr[i];
+        }
+    }
+    return secMax;
+}
+console.log(secondLargest([10, 5, 20, 8, 30]));
